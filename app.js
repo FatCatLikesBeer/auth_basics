f735585c71929be20d9da722f889595b6cb4554f
Billy Bagel <itisbilly@gmail.com>
2024-03-10 05:33:09 -0700

    Finished the tutorial

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-/////// app.js
-
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
@@ -7,14 +5,17 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
+//// ------ MongoDB Stuff ------ ////
 // MongoDB connection URI passed as environment variable
 const mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB);
+// Connect to DB
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDb connection error"));
 
-// MongoDB model
+// MongoDB 'User' model
 const User = mongoose.model(
   "User",
   new Schema({
@@ -23,18 +24,59 @@ const User = mongoose.model(
   })
 );
 
+//// ------ General App Stuff ------ ////
+// Instantiate our app :)
 const app = express();
 // Seting path & type of template
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
 // I think this is for cookies
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
 // This is for authentication.
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-// Routing & routing logic
+//// ------ Passport Stuff ------ ////
+// Passport.js local login 'strategy'
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      const match = await bcrypt.compare(password, user.password);
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      };
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      };
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);
+// The method below creates a user cookie which
+// allows them to stay logged in.
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+// The method below checks the cookie.
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch(err) {
+    done(err);
+  };
+});
+
+//// ------ Routing Stuff ------ ////
+// Middleware to pass 'currentUser' into all views.
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+// Instantiate Views
 app.get("/", (req, res) => {
   res.render("index", { user: req.user });
 });
@@ -43,12 +85,14 @@ app.get("/fail", (req, res) => res.render("fail"));
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.post("/sign-up", async (req, res, next) => {
   try {
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password,
-    });
-    const result = await user.save();
-    res.redirect("/");
+    bcrypt.hash(req.body.password, 10, async(err, hashedPassword) => {
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      });
+      const result = await user.save();
+      res.redirect("/");
+    })
   } catch(err) {
     return next(err);
   };
@@ -66,36 +110,8 @@ app.get("/log-out", (req, res, next) => {
   });
 });
 
-// Passport.js local login 'strategy'
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await User.findOne({ username: username });
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      };
-      if (user.password != password) {
-        return done(null, false, { message: "Incorrect password" });
-      };
-      return done(null, user);
-    } catch(err) {
-      return done(err);
-    }
-  })
-);
-// Passport.js cookie stuff
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch(err) {
-    done(err);
-  };
-});
-
-//app.listen(3000, () => console.log("App listening on port 3000!"));
+// Below is commented because /bin/www already runs a listen method.
+// app.listen(3000, () => console.log("App listening on port 3000!"));
 
 module.exports = app;
+
